3ee02656209b0a251cceecbf99c2834bec5f3081
warith <warith.ad02@gmail.com>
2023-01-04 00:55:40 +0100

    Implement Header and Footer components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
 import styled from "styled-components";
 
 const Header =()=>{
-    <HeaderContainer>
-        
-    </HeaderContainer>
+    return(
+        <HeaderContainer>
+            <h1>Memory Card</h1>
+        </HeaderContainer>
+    )
 }
 
 const HeaderContainer = styled.div`
+    background-color: #166534;
+    color: black;
+    font-size: 2vw;
+    font-family: arial;
+    color: white;
+    padding: 2rem 0;
+    text-align: center;
 `;
 
 export default Header;
--- src/themes/GlobalStyle.js
+++ src/themes/GlobalStyle.js
@@ -9,6 +9,16 @@ const GlobalStyle = createGlobalStyle`
         padding: 0;
     }
 
+    .App{
+        min-height: 100vh;
+        min-width: 100vw;
+        background-color: #f0fdf4;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+
 `
 
 export default GlobalStyle;
