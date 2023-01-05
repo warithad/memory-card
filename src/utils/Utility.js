24cbf0632922e1740a7df06bb0b7b1c71a4cfa25
warith <warith.ad02@gmail.com>
2023-01-05 23:29:32 +0100

    Create utility methods for shuffling and capitalizing

---
+++ src/utils/Utility.js
@@ -0,0 +1,24 @@
+
+const shuffle =(array)=>{
+
+    for(let i = 0; i < array.length; i++){
+        array = swap(array, i, Math.floor(Math.random() * array.length));
+    }
+
+    return array;
+}
+
+const swap =(array, first, second)=>{
+    const temp = array[first];
+    array[first] = array[second];
+    array[second] = temp;
+    
+    return array;
+}
+
+const capitalizeFirstLetter=(word)=>{
+    const firstLetter = word.charAt(0);
+    return word.replace(firstLetter, firstLetter.toUpperCase());
+}
+
+export {shuffle, capitalizeFirstLetter};
