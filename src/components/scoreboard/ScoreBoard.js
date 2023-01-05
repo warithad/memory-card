c41198181f9a4cc389ee5047498fde1697145de0
warith <warith.ad02@gmail.com>
2023-01-05 23:48:32 +0100

    Implement scoreboard

--- src/components/scoreboard/ScoreBoard.js
+++ src/components/scoreboard/ScoreBoard.js
@@ -1,7 +1,16 @@
+import styled from "styled-components";
 
-const ScoreBoard =()=>{
-
+const ScoreBoard =({highScore, currentScore})=>{
+    return(
+        <ScoreBoardWrapper>
+            <h2>High Score: {highScore}</h2>
+            <h2>Current Score: {currentScore}</h2>
+        </ScoreBoardWrapper>
+    );
 }
 
+const ScoreBoardWrapper = styled.div`
+    display: flex;
+`
 
 export default ScoreBoard;
